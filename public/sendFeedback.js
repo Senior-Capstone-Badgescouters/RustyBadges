0c34ad1f614a2090d5f08da951dd5f2c10ed7ac9
Nick Wylie <98427775+NickHubGitHub@users.noreply.github.com>
2023-11-08 14:27:57 -0600

    Contact form functionality
    User input from the contact form gets sent into a txt file (feedback.txt)

--- public/sendFeedback.js
+++ public/sendFeedback.js
@@ -1,37 +1,30 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+// function on the contact form to send in the users feedback into a txt file. 
+
 const fs = require('fs');
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
+app.use(express.static('public'));
 app.post('/save-feedback', (req, res) => {
   const email = req.body.email;
   const message = req.body.message;
 
   const feedbackContent = `Email: ${email}\nMessage: ${message}\n`;
 
-  // Write the content to a text file
   fs.appendFile('feedback.txt', feedbackContent, (err) => {
     if (err) {
       console.error(err);
-      res.status(500).json({ error: 'Error saving feedback.' });
+      res.status(500).send('Error saving feedback.');
     } else {
       console.log('Feedback saved successfully.');
-      res.status(200).json({ success: 'Feedback saved successfully.' });
+      res.status(200).send('Feedback saved successfully.');
     }
   });
 });
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
+const port = 3000; 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
