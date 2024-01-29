fd1a2e253d48894534c44959556b74f1812d2c12
Hersh Krishna <hershkrishna@protonmail.ch>
2024-01-29 10:42:43 -0600

    Implement post and get badge by id functions.

--- app.js
+++ app.js
@@ -19,6 +19,45 @@ app.get('/api/get-badges-by-level/:level', (req, res) => {
   get_badges_by_level_proc.all(req.params.level, (err, badges) => res.json(badges))
 });
 
+const get_badge_by_id_proc = db.prepare("select * from badges where \"badge_id\" == (?)");
+
+app.get('/api/get-badge-by-id/:id', (req, res) => {
+  get_badge_by_id_proc.get(req.params.id, (err, badge) => res.json(badge))
+});
+
+app.get('/api/get-all-badges', (req, res) => {
+  db.all('select * from badges', (err, badges) => { res.json })
+})
+
+const post_badge = db.prepare(
+  "INSERT INTO badges (badge_name, level, date, description,requirements,category)" +
+  " VALUES (?, ?, ?, ?, ?, ?)");
+
+app.post('/api/post-badge', (req, res) => {
+  const name = req.body.name;
+  const level = req.body.level;
+  const date = req.body.date;
+  const description = req.body.description;
+  const requirements = req.body.requirements;
+  const category = req.body.category;
+
+  console.log(req.body)
+
+
+  //TODO: HERSH This currently fails because req comes in with a null body and a null
+  //params field. Not sure why. Might be a bug in post-man or in express or I'm
+  //doing something really stupid
+  post_badge.run(name, level, date, description, requirements, category, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error saving badge' });
+    } else {
+      console.log('Badge saves successfully');
+      res.status(200).json({ success: "Badge saved successfully" })
+    }
+  })
+})
+
 app.post('/save-feedback', (req, res) => {
   const email = req.body.email;
   const message = req.body.message;
