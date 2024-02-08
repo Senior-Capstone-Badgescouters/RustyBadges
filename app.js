c067141157be8288dc8079afe7b6ddef66b83c1b
Nick Wylie <98427775+NickHubGitHub@users.noreply.github.com>
2024-02-08 14:26:39 -0600

    connected database
    Database is connected and displayed into each badge category we have

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose(); 
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,15 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// SQLite database connection
+const dbPath = path.resolve(__dirname, 'girlsscout.db');
+const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error('Error opening database:', err.message);
+  } else {
+    console.log('Connected to the database.');
+  }
+});
 
 app.post('/save-feedback', (req, res) => {
   const email = req.body.email;
@@ -24,6 +34,21 @@ app.post('/save-feedback', (req, res) => {
     } else {
       console.log('Feedback saved successfully.');
       res.status(200).json({ success: 'Feedback saved successfully.' });
+    }
+  });
+});
+
+app.get('/badges', (req, res) => {
+
+  const query = 'SELECT * FROM badges';
+
+  
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.error('Error executing query:', err.message);
+      res.status(500).json({ error: 'Error fetching data.' });
+    } else {
+      res.status(200).json(rows); 
     }
   });
 });
